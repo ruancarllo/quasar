3aa5fa7c6f9d6313dc7ee2e1315b4a9c5a771b8c
Ruan Carllo Silva <ruan4ks@gmail.com>
2023-09-07 20:21:04 -0300

    💥 struc: Dispersão do projeto em mais componentes

--- source/components/university-card.tsx
+++ source/components/university-card.tsx
@@ -1,47 +1,19 @@
 import * as Preact from 'preact';
+
 import './university-card.scss';
 
 class UniversityCard extends Preact.Component<Properties> {
   render(properties: Properties) {
-    const name = standardizeUniversityName(properties.nonStandardizedName);
-
     return (
       <div class="university-card">
-        <a>{name}</a>
+        <a>{properties.standardizedName}</a>
       </div>
     )
   }
 }
 
-function standardizeUniversityName(nonStandardizedName: string): string {
-  const characters = [...nonStandardizedName];
-
-  for (let count = 0; count < characters.length; count++) {
-    if (count === 0) {
-      characters[count] = characters[count].toUpperCase();
-    }
-
-    else if (characters[count] === ' ' && characters[count + 1]) {
-      characters[count + 1] = characters[count + 1].toUpperCase();
-      count += 1;
-    }
-
-    else if (characters[count] === '-' && characters[count + 1] && characters[count + 2] && count + 2 === characters.length - 1) {
-      characters[count + 1] = characters[count + 1].toUpperCase();
-      characters[count + 2] = characters[count + 2].toUpperCase();
-      count += 2;
-    }
-
-    else {
-      characters[count] = characters[count].toLowerCase();
-    }
-  }
-
-  return characters.join('');
-}
-
 interface Properties {
-  nonStandardizedName: string,
+  standardizedName: string,
   url: string
 }
 
--- source/main.tsx
+++ source/main.tsx
@@ -1,29 +1,5 @@
 import * as Preact from 'preact';
 
-import UniversityCard from './components/university-card.tsx';
-import fetchUniversities from './utilities/fetch-universities.ts';
+import Homepage from './components/homepage.tsx';
 
-class Application extends Preact.Component {
-  render() {
-    return (
-      <div class="application">
-        <h1>Questium</h1>
-        <div class="university-cards"></div>
-      </div>
-    );
-  }
-}
-
-async function main() {
-  Preact.render(<Application/>, document.body);
-
-  const universityCards: Preact.VNode<UniversityCard>[] = new Array();
-
-  for (let university of await fetchUniversities()) {
-    universityCards.push(<UniversityCard nonStandardizedName={university.nonStandardizedName} url={university.url}/>);
-  }
-
-  Preact.render(universityCards, document.querySelector('.university-cards'));
-}
-
-main();
+Preact.render(<Homepage/>, document.body);
