dfeed5b786472b833c80613a7fde15ca55c56b6a
Ruan Carllo Silva <ruan4ks@gmail.com>
2023-09-07 23:08:41 -0300

    🎯 feats: Inicialização da criação do algoritmo de embelezamento de imagens de questões

--- source/main.tsx
+++ source/main.tsx
@@ -2,4 +2,6 @@ import * as Preact from 'preact';
 
 import Homepage from './components/homepage.tsx';
 
+import beautifyQuestionImage from './utilities/beautify-question-image.ts';
+
 Preact.render(<Homepage/>, document.body);
