05841c49cb3aa5f856cf7991385aea6515bb93ad
Ruan Carllo Silva <ruan4ks@gmail.com>
2023-09-09 14:51:05 -0300

    🛠️ fixes: Renderização das imagens de questões ordenadamente

--- source/components/question-card.tsx
+++ source/components/question-card.tsx
@@ -6,22 +6,47 @@ import getRenderedComponent from '../helpers/get-rendered-component';
 
 class QuestionCard extends Preact.Component<Properties> {
   reference: Preact.RefObject<HTMLImageElement>;
+  index: number;
 
   constructor(properties: Properties) {
     super(properties);
+
+    if (!globalThis.renderedQuestionCard) {
+      globalThis.renderedQuestionCard = true;
+      globalThis.questionCardCount = 0;
+      globalThis.questionCardPromises = new Array();
+      globalThis.questionCardPromiseResolvers = new Array();
+    }
+
     this.reference = Preact.createRef();
+    this.index = globalThis.questionCardCount++;
+
+    let cardPromiseResolver: (value: unknown) => void;
+    let cardPromise = new Promise((resolve) => cardPromiseResolver = resolve);
+
+    globalThis.questionCardPromises[this.index] = cardPromise;
+    globalThis.questionCardPromiseResolvers[this.index] = cardPromiseResolver;
   }
 
   render() {
     return (
-      <img ref={this.reference} style={Styles.QuestionCard} class="question-card"/>
+      <img ref={this.reference} style={Styles.QuestionCard} class="question-card" id={this.index.toString()}/>
     );
   }
 
   async componentDidMount() {
+    this.reference.current.onload = () => {
+      globalThis.questionCardPromiseResolvers[this.index]();
+      globalThis.questionCardPromiseResolvers[this.index] = undefined;
+    }
+
     try {
       const randomQuestionUrl = await getRandomQuestionUrl(this.props.universityUrl);
       const beautifiedQuestionImage = await beautifyQuestionImage(randomQuestionUrl);
+
+      for (let promiseCount = 0; promiseCount < this.index; promiseCount++) {
+        await globalThis.questionCardPromises[promiseCount];
+      }
 
       this.reference.current.src = beautifiedQuestionImage.src;
     }
