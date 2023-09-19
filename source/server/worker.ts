857f6e1a90cc1fea3abf8f43d0f9715528d31161
Ruan Carllo Silva <ruan4ks@gmail.com>
2023-09-19 18:46:35 -0300

    💥 struc: Transformação do projeto em um mínimo produto viável (MVP), removendo o React

---
+++ source/server/worker.ts
@@ -0,0 +1,59 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+class Server {
+  public static async handleRequest(request: Request): Promise<Response> {
+    const requestURL = new URL(request.url);
+
+    if (requestURL.pathname.startsWith('/random-question')) {
+      const searchParameters = new URLSearchParams(requestURL.search);
+      const universityName = searchParameters.get('universityName');
+
+      if (!universityName) return new Response(null, {status: 500});
+      if (!universitiiesData[universityName]) return new Response(null, {status: 404});
+
+      const randomIndex = Math.floor(Math.random() * universitiiesData[universityName].length)
+      const randomQuestionURL = universitiiesData[universityName][randomIndex];
+
+      const imageRequest = await fetch(randomQuestionURL);
+      if (imageRequest.status !== 200) return new Response(null, {status: 500});
+
+      const imageBlob = await imageRequest.blob();
+
+      return new Response(imageBlob);
+    }
+
+    if (requestURL.pathname.startsWith('/quasar')) {
+      const requestPath = requestURL.pathname.replace('/quasar', '');
+
+      if (requestPath === '' || requestPath === '/') {
+        const resultHTML = Bun.file('source/client/home.html').stream();
+        return new Response(resultHTML);
+      }
+
+      if (requestPath.startsWith('/questions')) {
+        const resultHTML = Bun.file('source/client/questions.html').stream();
+        return new Response(resultHTML);
+      }
+    }
+
+    return new Response(undefined, {status: 404});
+  }
+
+  public static port = 8080;
+}
+
+const universitiiesData: {[universityName: string]: Array<string>} = {};
+const universityDataPathes = fs.readdirSync('source/server/data');
+
+for (const relativePath of universityDataPathes) {
+  const universityDataPath = path.join('source/server/data', relativePath);
+  const universityName = relativePath.replace(/\.json$/g, '');
+
+  const universityRawData = fs.readFileSync(universityDataPath, 'utf-8');
+  const universityParsedData = JSON.parse(universityRawData);
+
+  universitiiesData[universityName] = universityParsedData;
+}
+
+Bun.serve({port: Server.port, fetch: Server.handleRequest});
